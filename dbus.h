41466f53ab5338df8135890eea8ffbc88c2a0d90
sam-at-github <sgpinkus@gmail.com>
2014-07-07 13:25:13 +1000

    Oops need that header.

---
+++ dbus.h
@@ -0,0 +1,8 @@
+#ifndef DBUS_H
+#define DBUS_H
+#define DBUS_SERVICE "com.epicmorsel.dbus"
+#define DBUS_INTERFACE "com.epicmorsel.dbus.gateway"
+#define DBUS_OBJECT_PATH "/com/epicmorsel/dbus/gateway"
+#define DBUS_RETRIES 5
+#endif // DBUS_H
+
