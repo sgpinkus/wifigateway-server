54b218d778b1a07d1506fd79a0b5332e64904a4a
sam-at-github <sgpinkus@gmail.com>
2014-07-12 11:43:44 +1000

    Add postinit prerm and rm .ex crud. Fix daemon.

--- debian/wifigateway-server.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the wifigateway-server package
-#
-0 4	* * *	root	[ -x /usr/bin/wifigateway-server_maintenance ] && /usr/bin/wifigateway-server_maintenance
--- debian/wifigateway-server/usr/bin/wifigateway-script/gw_reset_iptables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Used for testing.
-cd `dirname $0`
-source gw_functions.sh
-gw_reset_iptables $*
